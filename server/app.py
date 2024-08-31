0b6a4576954adf72f8ffa09819bc435956c9e2e4
johney <johneyburke@gmail.com>
2024-08-31 13:52:46 -0600

    flask running

--- server/app.py
+++ server/app.py
@@ -3,11 +3,15 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import Flask, request
 from flask_restful import Resource
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 # Local imports
-from config import app, db, api
+from config import db, api
 # Add your model imports
 
 
@@ -15,7 +19,7 @@ from config import app, db, api
 
 @app.route('/')
 def index():
-    return '<h1>Project Server</h1>'
+    return '<h1>TV Show App</h1>'
 
 
 if __name__ == '__main__':
